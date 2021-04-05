6a1412e8152655f13e6476d8d5bad2f39f21a9f6
sainath biradar <ajay@Ajays-MacBook-Pro.local>
2021-04-06 09:58:59 +1000

    intial commit

--- lib/app.rb
+++ lib/app.rb
@@ -1,3 +1,23 @@
-class App {
-    
-}
+class App 
+    attr_accessor :tasks
+    def initialize 
+        @tasks = []
+    end
+
+    def add_task(task_input)
+    @tasks << { task: task_input, completed: false }
+    end
+
+    def display_add_task
+        puts 'Enter the task name'
+    end
+    def task_add
+        gets.strip
+    end
+    def display_tasks
+        puts "Tasks:"
+        @tasks.each_with_index do |task,index|
+        puts "#{index + 1}. #{task[:task]} [ ]"
+        end
+    end
+end     
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,11 +1,45 @@
 require_relative '../lib/app'
+
 RSpec.describe App do
-        subject(:app){
-           describe_class.new
-        }
-        it 'should be an intance of app' do
-            # expect(subject).to be_a App  #method  is use for start a test
-        end
-     
-        
+    subject(:app){
+        described_class.new
+    }
+    it 'should be instance of App' do
+        expect(app).to be_a App
     end
+
+    it 'should have an empty task list' do
+        expect(app.tasks).to eq []
+    end
+        context 'adding a task' do
+            it 'should be able to add a task' do
+                app.add_task('Create a terminal app')
+                expect(app.tasks.last).to eq({ task: 'Create a terminal app', completed: false })
+            
+        end
+        it 'should ask user the input' do
+            expected_output = "Enter the task name\n"
+             expect{ app.display_add_task }.to output(expected_output).to_stdout
+        end
+
+        let(:input) { StringIO.new('test task') } 
+        it 'should be albe to receive task from terminal'do
+          $stdin = input
+          expect(app.task_add).to eq('test task')  
+        end
+       
+
+    end
+
+    context 'display tast' do
+        before(:each) do
+            app.tasks = [{tasks: 'test task', completed: false}]
+        end
+        it 'should display all tasks' do
+            expected_output = "Tasks:\n1. test task [ ]\n"
+            expect{ app.display_tasks }.to output(expected_output).to_stdout
+        end
+    end
+
+    
+end
